b336e98bc40258bce9e0faa5022b60016b2c0818
Davesuy <davesuy@gmail.com>
2019-07-09 20:39:07 +0800

    vue integration

--- resources/js/frontend/app.js
+++ resources/js/frontend/app.js
@@ -22,8 +22,8 @@ window.Vue = Vue;
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+Vue.component('button-sample', require('./components/MyButton.vue').default);
+Vue.component('Articles', require('./components/Articles.vue').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
